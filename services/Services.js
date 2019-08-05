654e4ec388ca71f09a0e12e0c58d1f49d4255c05
sanjayparidar <sanjaypatidar402@gmail.com>
2019-08-05 15:50:35 +0530

    5 agust

--- services/Services.js
+++ services/Services.js
@@ -324,7 +324,7 @@ router.post('/order',(req,res)=>{
 
 router.post('/order/search',(req,res)=>{
     
-    Order.find({$and: [{CurrentStatus:req.body.CurrentStatus},{$or:[{'Giver_Phone': {'$regex': req.body.search,'$options': 'i'}},{'Name': {'$regex': req.body.search,'$options': 'i'}}]}]},function(err, results) { 
+    Order.find({$and: [{CurrentStatus:req.body.CurrentStatus},{$or:[{'Name': {'$regex': req.body.search,'$options': 'i'}},{'Charge_id': {'$regex': req.body.search,'$options': 'i'}},{'Phone': {'$regex': req.body.search,'$options': 'i'}},{'Email': {'$regex': req.body.search,'$options': 'i'}},{'Commodity': {'$regex': req.body.search,'$options': 'i'}},{'Giver_Name': {'$regex': req.body.search,'$options': 'i'}},{'Giver_Phone': {'$regex': req.body.search,'$options': 'i'}},{'Giver_Email': {'$regex': req.body.search,'$options': 'i'}},{'Recevier_Phone': {'$regex': req.body.search,'$options': 'i'}},{'Recevier_Name': {'$regex': req.body.search,'$options': 'i'}},{'Recevier_Email': {'$regex': req.body.search,'$options': 'i'}},{'Price': {'$regex': req.body.search,'$options': 'i'}}]}]},function(err, results) { 
        if(err){
         res.status(400).json({response:"1",error:"error in driver"});
 
